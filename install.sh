09566bf3c11af26f412640941a203af79d954232
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2022-04-18 11:25:56 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+set -e
+
+# This script was downloaded from https://github.com/qzind/qz.sh and is part of the QZ Tray product
+# Contribute to this script by visting the above website and opening an issue or pull request
+
+echo "Hello world"
