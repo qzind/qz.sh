3f2b4c9678c9c8dd7f284be501b4fce9c95c1430
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2022-04-18 13:06:38 -0400

    Test 3

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ set -e
 # This script was downloaded from https://github.com/qzind/qz.sh and is part of the QZ Tray product
 # Contribute to this script by visting the above website and opening an issue or pull request
 
-echo "Hello world"
+# Console colors
+RED="\\x1B[1;31m";GREEN="\\x1B[1;32m";YELLOW="\\x1B[1;33m";PLAIN="\\x1B[0m"
+
+echo -e "${GREEN}Hello world${PLAIN}"
